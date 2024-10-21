1576a7280c511e182b08c5d0554b11b18c150ca4
BasileDS <78741059+BasileDS@users.noreply.github.com>
2024-10-21 08:56:39 +0000

    display housing cards

--- src/components/HouseCard.jsx
+++ src/components/HouseCard.jsx
@@ -1,3 +1,16 @@
+import housing from "../data/housing.json"
+import "../styles/house-card.css"
+
 export function HouseCard () {
-    return "hello"
+
+    return <div className="housing-wrapper">
+        {housing.map( house => {
+            return <article className="house-card">
+                    <img src={house.pictures[0]} />
+                    <div className="title-background">
+                        <p className="house-title">{house.title}</p>
+                    </div>
+                </article>
+        })}
+    </div>
 }
